111ce4bffaf5387545c04568254a10cb89a7ab89
Hau-Wei Lin <linhoway@gmail.com>
2023-02-21 22:58:51 +0800

    add bsp_i2c.c/h

---
+++ I2C/User/I2C/bsp_i2c.c
@@ -0,0 +1,6 @@
+#include "bsp_i2c.h"
+
+void bsp_i2c_init(void){
+	
+	
+}
---
+++ I2C/User/I2C/bsp_i2c.h
@@ -0,0 +1,8 @@
+#ifndef __BSP_I2C_H_
+#define __BSP_I2C_H_
+
+#include "stm32f4xx_i2c.h"
+
+void bsp_i2c_init(void); 
+
+#endif /* __BSP_I2C_H_ */
